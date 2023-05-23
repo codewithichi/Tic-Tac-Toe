9ec20080679cad79436e2cb299d6f5c46bba313e
Rikki Maligaya <rikkimaligaya@gmail.com>
2023-05-24 02:40:52 +0800

    Made Tic Tac Toe Game

--- index.js
+++ index.js
@@ -1,72 +1,153 @@
-let playerText = document.getElementById('playerText')
-let restartBtn = document.getElementById('restartBtn')
-let boxes = Array.from(document.getElementsByClassName('box'))
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const previousBtn = document.querySelector("#previousBtn");
+const nextBtn = document.querySelector("#nextBtn");
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+let moveHistory = [];
+let currentMoveIndex = -1;
 
-let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+initializeGame();
 
-const O_TEXT = "O"
-const X_TEXT = "X"
-let currentPlayer = X_TEXT
-let spaces = Array(9).fill(null)
-
-const startGame = () => {
-    boxes.forEach(box => box.addEventListener('click', boxClicked))
+function initializeGame() {
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
+    previousBtn.addEventListener("click", showPreviousMove);
+    nextBtn.addEventListener("click", showNextMove);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+    resetMoveHistory();
+    updateMoveHistory();
 }
 
-function boxClicked(e) {
-    const id = e.target.id
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
 
-    if(!spaces[id]){
-        spaces[id] = currentPlayer
-        e.target.innerText = currentPlayer
+    if (options[cellIndex] !== "" || !running) {
+        return;
+    }
 
-        if(playerHasWon() !==false){
-            playerText.innerHTML = `${currentPlayer} has won!`
-            let winning_blocks = playerHasWon()
+    updateCell(this, cellIndex);
+    checkWinner();
+    updateMoveHistory();
+}
 
-            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
-            return
+function updateCell(cell, index) {
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
+function changePlayer() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner() {
+    let roundWon = false;
+
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if (cellA === "" || cellB === "" || cellC === "") {
+            continue;
         }
+        if (cellA === cellB && cellB === cellC) {
+            roundWon = true;
+            break;
+        }
+    }
 
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+    if (roundWon) {
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    } else if (!options.includes("")) {
+        statusText.textContent = `Draw!`;
+        running = false;
+    } else {
+        changePlayer();
     }
 }
 
-const winningCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+function restartGame() {
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => (cell.textContent = ""));
+    running = true;
+    resetMoveHistory();
+    updateMoveHistory();
+}
 
-function playerHasWon() {
-    for (const condition of winningCombos) {
-        let [a, b, c] = condition
+function resetMoveHistory() {
+    moveHistory = [];
+    currentMoveIndex = -1;
+    previousBtn.disabled = true;
+    nextBtn.disabled = true;
+}
 
-        if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
-            return [a,b,c]
-        }
+function updateMoveHistory() {
+    const move = [...options];
+    moveHistory.push(move);
+    currentMoveIndex = moveHistory.length - 1;
+
+    if (moveHistory.length > 1) {
+        previousBtn.disabled = false;
     }
-    return false
+
+    if (currentMoveIndex < moveHistory.length - 1) {
+        nextBtn.disabled = false;
+    }
 }
 
-restartBtn.addEventListener('click', restart)
+function showPreviousMove() {
+    if (currentMoveIndex > 0) {
+        currentMoveIndex--;
+        const previousMove = moveHistory[currentMoveIndex];
+        updateBoard(previousMove);
+    }
 
-function restart() {
-    spaces.fill(null)
+    if (currentMoveIndex === 0) {
+        previousBtn.disabled = true;
+    }
 
-    boxes.forEach( box => {
-        box.innerText = ''
-        box.style.backgroundColor=''
-    })
-
-    playerText.innerHTML = 'Tic Tac Toe'
-
-    currentPlayer = X_TEXT
+    nextBtn.disabled = false;
 }
 
-startGame()
+function showNextMove() {
+    if (currentMoveIndex < moveHistory.length - 1) {
+        currentMoveIndex++;
+        const nextMove = moveHistory[currentMoveIndex];
+        updateBoard(nextMove);
+    }
+
+    if (currentMoveIndex === moveHistory.length - 1) {
+        nextBtn.disabled = true;
+    }
+
+    previousBtn.disabled = false;
+}
+
+function updateBoard(move) {
+    options = move;
+    cells.forEach((cell, index) => {
+        cell.textContent = move[index];
+    });
+}
+
+restartGame();
+
